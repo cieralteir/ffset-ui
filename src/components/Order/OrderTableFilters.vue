0c5afb6343c814a2f7e38b82a40d5ac806d67813
John Carlo Ricana <cier.alteir@gmail.com>
2020-07-26 03:26:54 +0800

    Order table filters
    Signed-off-by: John Carlo Ricana <cier.alteir@gmail.com>

---
+++ src/components/Order/OrderTableFilters.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="order-table-filters">
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <b-field>
+          <b-input
+            v-model="filters.search"
+            placeholder="Search..."
+            type="search"
+            icon="magnify"
+          />
+        </b-field>
+      </div>
+      <div class="column is-4">
+        <b-field label="Created Date">
+          <b-datepicker
+            v-model="inputs.createdDate"
+            placeholder="Select a date..."
+            range
+          />
+        </b-field>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    filters: {
+      search: "",
+      createdDateStart: "",
+      createdDateEnd: ""
+    },
+    inputs: {
+      createdDate: null
+    }
+  }),
+  watch: {
+    filters: {
+      handler(value) {
+        this.$emit("filter", value);
+      },
+      deep: true
+    },
+    "inputs.createdDate"(value) {
+      this.filters.createdDateStart = value[0];
+      this.filters.createdDateEnd = value[1];
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.order-table-filters {
+  margin-bottom: 30px;
+}
+</style>
--- src/pages/orders/index.vue
+++ src/pages/orders/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="page-orders">
+    <order-table-filters />
     <order-table />
   </div>
 </template>
 
 <script>
 import OrderTable from "@/components/Order/OrderTable";
+import OrderTableFilters from "@/components/Order/OrderTableFilters";
 
 export default {
   components: {
-    OrderTable
+    OrderTable,
+    OrderTableFilters
   }
 };
 </script>
