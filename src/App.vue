c5342b9702339f9ab7be611ef348eef9f5ddd5cb
John Carlo Ricana <cier.alteir@gmail.com>
2020-07-26 01:53:18 +0800

    Install buefy
    Signed-off-by: John Carlo Ricana <cier.alteir@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <the-header />
+    <div class="container is-fluid">
+      <div class="container">
+        <div class="notification">
+          This container is <strong>centered</strong> on desktop.
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from "@/components/TheHeader";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TheHeader
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        FSSET-UI
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
